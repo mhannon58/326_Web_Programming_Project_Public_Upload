00557ddad08606fa53318472d3de8fb94126e56d
Ishan Bhardwaj <ibhardwaj@umass.edu>
2023-04-24 18:32:36 -0400

    Connected signup and login pages with PouchDB

---
+++ frontend_js/login.js
@@ -0,0 +1,14 @@
+import { Crud } from './pouchdb.js';
+
+// connects to existing user database
+let loginObj = new Crud('signup_db');
+let userData = null;
+
+let loginButton = document.getElementById("login-button");
+loginButton.addEventListener("click", async function(event) {
+  // will make sure of its usage later
+  event.preventDefault();
+  
+  let username = document.getElementById("username").value;
+  userData = await loginObj.readDoc(username);
+});
---
+++ frontend_js/signup.js
@@ -0,0 +1,23 @@
+import { Crud } from './pouchdb.js';
+
+const signupObj = new Crud('signup_db');
+
+let registerButton = document.getElementById("register-button");
+registerButton.addEventListener("click", async function(event) {
+  // will make sure of its usage later
+  event.preventDefault();
+
+  let username = document.getElementById("username").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let doc = {
+      _id: username,
+      email: email,
+      password: password,
+      createdPosts: [],
+      acceptedPosts: []
+  }
+
+  await signupObj.createDoc(doc);
+  let stored = await signupObj.readDoc(username);
+});
