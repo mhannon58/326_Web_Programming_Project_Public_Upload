7ca232f14f8a1a406b4cddea3e2bbacc7c3b0982
Max Hannon <mhannon@umass.edu>
2023-05-08 19:05:16 -0400

    can serve the home page

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,11 @@
 
 import express from "express";
 import logger from "morgan";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
@@ -11,7 +16,11 @@ app.use(logger("dev"));
 
 app.use(express.json());
 
-app.use(express.static("src/client"));
+app.use(express.static("326-project-repo-team-44/"));
+
+app.get('/',function(req,res) {
+  res.sendFile('src/client/home.html', {root: './326-project-repo-team-44'});
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
