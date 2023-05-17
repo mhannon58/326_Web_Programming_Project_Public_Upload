ba80f4fcfd5ea54f634380674a05c1d0446d7ba7
Alec Xie <adxie@umass.edu>
2023-05-16 21:13:48 -0400

    Feedback upon accepting post

--- src/client/listing.js
+++ src/client/listing.js
@@ -4,7 +4,7 @@ acceptButton.addEventListener('click', async () => {
   const path = window.location.pathname;
   const post_id = path.substring(path.lastIndexOf('/')+1);
   const accept_id = localStorage.getItem('curr_user');
-  const data = { 
+  const pack = { 
     post_id: post_id, 
     accept_id: accept_id 
   };
@@ -15,12 +15,18 @@ acceptButton.addEventListener('click', async () => {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(pack)
   });
 
+<<<<<<< HEAD
   const status = await response.json();
 
   if (status["status"] === "failure") {
     alert('Post can no longer be accepted.');
   }
+=======
+  const data = await response.json()
+  alert(data.message);
+
+>>>>>>> fa0f9ef (Feedback upon accepting post)
 });
